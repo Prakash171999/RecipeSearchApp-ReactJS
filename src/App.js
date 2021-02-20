b9c320af378637c6b371ae5f3e8195caa3d6a541
Prakash Rai <prr0154@my.londonmet.ac.uk>
2021-02-20 12:43:14 +0545

    changes in css files. Recipe component changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 import Helpers from "./Helpers";
+import Recipe from "./Components/Recipe";
 import Axios from "axios";
 
-//variable that imports the key, id and base from helpers 
+//variable that imports the key, id and base from helpers
 const api = {
   key: Helpers.APP_KEY,
   id: Helpers.APP_ID,
@@ -13,14 +15,14 @@ const api = {
 const App = () => {
   //state to store the input field data
   const [query, setQuery] = useState("");
-  //state to store the array of recipes from api
-  const [recipes, setRecipes] = useState([]);
+  //state to store the array of RecipesData from api
+  const [recipeData, setRecipeData] = useState([]);
 
   //URL for fetching recipe api. Template literals are used. Fetching the data using axios with async and await
   const url = `${api.base}search?q=${query}&app_id=${api.id}&app_key=${api.key}`;
   const getData = async () => {
     const result = await Axios.get(url);
-    setRecipes(result.data.hits);
+    setRecipeData(result.data.hits);
     console.log(result);
     setQuery("");
   };
@@ -36,25 +38,28 @@ const App = () => {
 
   return (
     <div>
-      <h1>Food Searching App</h1>
-      <form className="search-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Search Food"
-          onChange={handleText}
-          autoComplete="off"
-          value={query}
-        />
-        <button type="sumbit" value="search">
-          Search
-        </button>
-      </form>
+      <div className="jumbotron">
+        <div className="container">
+          <h1>Search Food Recipes</h1>
+          <div className="search">
+            <form className="search-form" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Search Food"
+                onChange={handleText}
+                autoComplete="off"
+                value={query}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
       {/* show recipes div. conditional rendering. If the recipes array is not empty map the recipe and display it. */}
       <div className="recipes">
-        {recipes !==[] &&
-        recipes.map(recipe =>
-          <h2>{recipe.recipe.label}</h2>
-        )}
+        {recipeData !== [] &&
+          recipeData.map((recipes) => (
+            <Recipe key={uuidv4()} recipe={recipes} />
+          ))}
       </div>
     </div>
   );
--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
- const Recipe = () => {
+ const Recipe = (props) => {
+       //grabbing the label, image, url and ingredients from recipes using destructuring.
+       const {label, image, url, ingredients} = props.recipe.recipe;
+       console.log(props.recipe.recipe);
       return (
-            <div>
-                  
+            <div className="recipe">
+                  <h2>{label}</h2>
+                  <img src={image} alt={label} />
+                  <a href={url} target="_blank" rel="noopenernoreferrer">
+                        URL
+                  </a>
+                  <button>Ingredients</button>
             </div>
       )
 }
